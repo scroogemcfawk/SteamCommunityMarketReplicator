e270459fd0a33175d862b6f5eabcee14443902ea
scroogemcfawk <scroogemcfawk@gmail.com>
2023-04-18 16:26:21 +0700

    CSConfigConverter optimized parsing time from 2m to 1s
    Took 48 seconds

--- src/main/kotlin/utils/CSConfigConverter.kt
+++ src/main/kotlin/utils/CSConfigConverter.kt
@@ -1,76 +1,57 @@
 package utils
 
+import org.json.JSONObject
 import java.io.File
 import java.io.FileNotFoundException
 import java.util.Stack
 import kotlin.Exception
 
 
-private const val DEFAULT_SOURCE = "D:/SteamLibrary/steamapps/common/Counter-Strike Global Offensive/csgo/scripts/items/items_game" +
-        ".txt"
-private const val DEFAULT_TARGET = "C:/Users/scroo/IdeaProjects/RePricerDBM/src/main/resources/game/items_game.json"
+private val DEFAULT_SOURCE = System.getenv("CSITEMS")
+private val DEFAULT_TARGET = System.getProperty("user.dir") + "/src/main/resources/game/items_game.json"
 
 
 /**
  * Converts items_game.txt CS:GO config file to json format.
  */
-class CSConfigConverter(
-    source: String = DEFAULT_SOURCE,
-    target: String = DEFAULT_TARGET,
-) {
-    private val fr: File
-    private val to: File
+class CSConfigConverter(source: String = "", dest: String = "") {
+    val fr: File
+    val to: File
 
     init {
-        fr = File(source)
-        if (!fr.exists()) throw FileNotFoundException()
-        to = File(target)
+        fr = File(source).takeIf { it.exists() && it.isFile } ?: File(DEFAULT_SOURCE).takeIf { it.exists() && it.isFile }
+                ?: throw FileNotFoundException("Config source file not found.")
+        to = File(dest).takeIf { dest.isNotBlank() } ?: File(DEFAULT_TARGET).takeIf {
+            File(
+                it.absolutePath.split("\\").dropLast(1).joinToString("\\")
+            ).isDirectory
+        } ?: File("./items_game.json")
     }
 
     private val tab = "\t"
 
     fun run() {
-        // TODO: optimize with StringBuilder? because long concat and current time: 2m 5s
-        val stack = Stack<HashMap<String, Int>>()
-        stack.add(HashMap())
-        var res = ""
-        res += "{\n"
-        var cl = 1
+        val json = JSONObject()
+        val keyChain = Stack<String>()
         for (line in fr.bufferedReader().lines()) {
-            val qcount = line.count { it == '\"' }
-
-            when (qcount) {
+            when (line.count { it == '\"' }) {
                 0 -> {
-                    if (line.trim() == "{") {
-                        ++cl
-                        res += "{\n"
-                        stack.add(HashMap())
-                    }
                     if (line.trim() == "}") {
-                        --cl
-                        if (res.takeLast(2) == ",\n") {
-                            res = res.dropLast(2) + "\n"
-                        }
-                        res += tab.repeat(cl) + "}" + if (cl > 1) ",\n" else "\n"
-                        stack.pop()
+                        keyChain.pop()
                     }
                 }
 
                 2 -> {
-                    val ks = stack.peek()
-                    val key = line.trim().replace("\"", "")
-                    val c = ks[key] ?: 0
-                    ks[key] = c + 1
-                    res += "${tab.repeat(cl)}\"$key${if (c > 0) c else ""}\": "
+                    val k = line.trim().replace("\"", "")
+                    val obj = json.getByKeyChain(keyChain)
+                    obj.putIfAbsent(k, JSONObject())
+                    keyChain.add(k)
                 }
 
                 4 -> {
-                    val ks = stack.peek()
                     val (k, v) = line.trim().split("\t\t").map { it.replace("\"", "") }
-                    val c = ks[k] ?: 0
-                    ks[k] = c + 1
-
-                    res += tab.repeat(cl) + "\"$k${if (c > 0) c else ""}\": \"$v\",\n"
+                    val obj = json.getByKeyChain(keyChain)
+                    obj.accumulate(k, v)
                 }
 
                 else -> {
@@ -78,8 +59,7 @@ class CSConfigConverter(
                 }
             }
         }
-        res += "}\n"
-        to.printWriter().use { it.println(res) }
+        to.printWriter().use { it.println(json.toString()) }
     }
 }
 
